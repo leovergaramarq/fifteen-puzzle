6e3fbc9bdf4f6101e744bbc2ee89b72a7b304e7d
Leonardo Vergara Márquez <leonardo11vergara11@gmail.com>
2023-08-14 19:33:06 -0500

    undo + redo options

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -1,13 +1,31 @@
 window.addEventListener(
   "load",
   function () {
+    // DOM elements
     const $tiles = [...document.querySelectorAll(".playable .tile")];
     const $tile16 = $tiles.find((tile) => tile.classList.contains("tile-16"));
-    let isPlaying = true;
-    let counter;
-    let record = -1;
+    const $restartBtn = document.querySelector(".restart");
+    const $solveBtn = document.querySelector(".solve");
+    const $undoBtn = document.querySelector(".undo");
+    const $redoBtn = document.querySelector(".redo");
+    const $counter = document.querySelector(".counter span");
+    const $record = document.querySelector(".record span");
 
-    startGame();
+    // game variables
+    let isPlaying;
+    let counter;
+    let record;
+    let history;
+    let historyIndex;
+
+    init();
+
+    // functions
+
+    function init() {
+      setRecord(-1);
+      newGame();
+    }
 
     function getOrderFromClassName(el) {
       return +el.className
@@ -16,10 +34,10 @@ window.addEventListener(
         .split("-")[1];
     }
 
-    function getTileOrder(el) {
-      return el.style.order !== ""
-        ? +el.style.order
-        : getOrderFromClassName(el);
+    function getTileOrder($elem) {
+      return $elem.style.order !== ""
+        ? +$elem.style.order
+        : getOrderFromClassName($elem);
     }
 
     function areTilesAdjacent(order1, order2) {
@@ -39,36 +57,137 @@ window.addEventListener(
       $tiles.forEach((tile, i) => (tile.style.order = i + 1));
     }
 
-    function order() {
+    function orderTiles() {
       $tiles.forEach(
         (tile) => (tile.style.order = getOrderFromClassName(tile))
       );
     }
 
-    function startGame() {
-      isPlaying = true;
-      counter = 0;
+    function solve() {
+      emptyHistory();
+      orderTiles();
+      updateHistory();
+      setCounter(-1);
+    }
+
+    function newGame() {
+      if (!isPlaying) isPlaying = true;
       shuffleTiles();
+      history = [getTilesOrder()];
+      historyIndex = 0;
+      setCounter(0);
+      setRecord(-1);
+    }
+
+    function emptyHistory() {
+      history.splice(0);
+      historyIndex = -1;
+    }
+
+    function setCounter(counter2) {
+      counter = counter2;
+      updateCounterHtml();
+    }
+
+    function setRecord(record2) {
+      record = record2;
+      updateRecordHtml();
+    }
+
+    function onClickTile(target) {
+      if (!isPlaying || target === $tile16) return;
+      const order = getTileOrder(target);
+      const order16 = getTileOrder($tile16);
+      if (areTilesAdjacent(order, order16)) {
+        $tile16.style.order = order;
+        target.style.order = order16;
+
+        updateHistory();
+        setCounter((counter === -1 ? 0 : counter) + 1);
+        checkWin();
+      }
+    }
+
+    function updateHistory() {
+      history.splice(historyIndex + 1);
+      history.push(getTilesOrder());
+      historyIndex = history.length - 1;
+    }
+
+    function updateCounterHtml() {
+      $counter.textContent = counter === -1 ? "-" : counter;
+    }
+
+    function updateRecordHtml() {
+      $record.textContent = record === -1 ? "-" : record;
+    }
+
+    function checkWin() {
+      if (!didWin()) return;
+
+      isPlaying = false;
+      const newRecord = checkNewRecord();
+      setTimeout(() => {
+        alert(`You won! ${newRecord ? `New record! ${record}` : ""}`);
+      }, 0);
+    }
+
+    function checkNewRecord() {
+      if (record === -1 || counter < record) {
+        setRecord(counter);
+        return true;
+      }
+      return false;
+    }
+
+    function getTilesOrder() {
+      return $tiles.map(($tile) => getTileOrder($tile));
+    }
+
+    function loadHistoryItem() {
+      const historyItem = history[historyIndex];
+      if (historyItem === undefined) {
+        console.log("historyItem is undefined");
+        return;
+      }
+      $tiles.forEach(($tile, i) => {
+        $tile.style.order = historyItem[i];
+      });
+    }
+
+    function undo() {
+      if (historyIndex === 0) return;
+      historyIndex--;
+      loadHistoryItem();
+      setCounter(counter - 1);
+    }
+
+    function redo() {
+      if (historyIndex === history.length - 1) return;
+      historyIndex++;
+      loadHistoryItem();
+      setCounter(counter + 1);
+    }
+
+    function parentEquals($elem, $parent) {
+      while ($elem !== $parent && $elem !== null) {
+        $elem = $elem.parentElement;
+      }
+      return $elem === $parent;
     }
 
     // click events
     this.document.addEventListener("click", function ({ target }) {
-      if ($tiles.includes(target) && isPlaying && target !== $tile16) {
-        const order = getTileOrder(target);
-        const order16 = getTileOrder($tile16);
-        if (areTilesAdjacent(order, order16)) {
-          $tile16.style.order = order;
-          target.style.order = order16;
-          counter++;
-
-          if (didWin()) {
-            // isPlaying = false;
-            if (counter < record) {
-              record = counter;
-            }
-            alert("You won!");
-          }
-        }
+      if ($tiles.includes(target)) {
+        onClickTile(target);
+      } else if (parentEquals(target, $restartBtn)) {
+        newGame();
+      } else if (parentEquals(target, $solveBtn)) {
+        solve();
+      } else if (parentEquals(target, $undoBtn)) {
+        undo();
+      } else if (parentEquals(target, $redoBtn)) {
+        redo();
       }
     });
   },
